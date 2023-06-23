57ec89dcaea6d4f5dae9afd03bd325540254c4fd
Anton Zvontcov <azvontcov@asginc.us>
2023-06-23 09:54:07 -0400

    adds some style to here section

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
 import styles from './page.module.css';
+import Image from 'next/image';
 
 const Contact = () => {
-  return <div className={styles.container}>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>Let&apos;s keep in touch</div>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            alt="contact"
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="text" placeholder="email" className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            placeholder="message"
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button url="#" text="send" className={styles.button}>
+            Send
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
