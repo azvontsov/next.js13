b61011557bfa9ebc7f09dc2c908d015eb3466d04
azvonsov <azvontsov@gmail.co>
2023-06-22 23:00:08 -0400

    adds folder structure

---
+++ src/app/blog/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogLayout = ({ children }) => {
+  return (
+    <div>
+      {/* <h1>This is blog</h1> */}
+      {children}
+    </div>
+  );
+};
+export default BlogLayout;
