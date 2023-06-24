0c0329a885465d5165d0953ebe5ed113dfdb9cab
Anton Zvontcov <azvontcov@asginc.us>
2023-06-24 12:06:54 -0400

    ready for mongoDB

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,41 +1,38 @@
 import React from 'react';
 import styles from './page.module.css';
 import Image from 'next/image';
-// import { notFound } from 'next/navigation';
+import { notFound } from 'next/navigation';
 
-// async function getData(id) {
-//   const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
-//     cache: 'no-store',
-//   });
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: 'no-store',
+  });
 
-//   if (!res.ok) {
-//     return notFound();
-//   }
+  if (!res.ok) {
+    return notFound();
+  }
 
-//   return res.json();
-// }
+  return res.json();
+}
 
-// export async function generateMetadata({ params }) {
-//   const post = await getData(params.id);
-//   return {
-//     title: post.title,
-//     description: post.desc,
-//   };
-// }
+export async function generateMetadata({ params }) {
+  const post = await getData(params.id);
+  return {
+    title: post.title,
+    description: post.desc,
+  };
+}
 
-const BlogPost = ({ params }) => {
-  // const data = await getData(params.id);
+const BlogPost = async ({ params }) => {
+  const data = await getData(params.id);
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
-          <h1 className={styles.title}>
-            {/* {data.title} */}
-            Test blog post title
-          </h1>
+          <h1 className={styles.title}>{data.title}</h1>
           <p className={styles.desc}>
-            {/* {data.desc} */}
-            Test blog post paragraph
+            {data.desc}
+            {data.body}
           </p>
           <div className={styles.author}>
             <Image
@@ -46,16 +43,13 @@ const BlogPost = ({ params }) => {
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>
-              {/* {data.username} */}
-              Test
-            </span>
+            <span className={styles.username}>{data.username}</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
           <Image
             // src={data.img}
-            src="/hero2.png"
+            src="/illustration.png"
             alt=""
             fill={true}
             className={styles.image}
@@ -64,18 +58,8 @@ const BlogPost = ({ params }) => {
       </div>
       <div className={styles.content}>
         <p className={styles.text}>
-          {/* {data.content} */}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-          quibusdam quia necessitatibus velit? Dolorem vero sint velit excepturi
-          eius suscipit praesentium laborum nostrum consequuntur, doloribus
-          perferendis qui placeat, quos minima. Consequatur mollitia asperiores
-          assumenda atque, voluptates, recusandae nihil maiores dolore impedit
-          fugiat optio dignissimos nemo dolores ullam tenetur totam qui dolorum
-          repellat enim facilis architecto. Rem dolorum reiciendis sint
-          nesciunt. Unde modi aliquam exercitationem pariatur ducimus. A
-          repellat veritatis quidem? Iste, odio minima? Sed, quia temporibus
-          quis voluptates laboriosam vitae minima nesciunt illo, molestias non
-          enim vel maxime quidem culpa!
+          {data.content}
+          {data.body}
         </p>
       </div>
     </div>
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,78 +3,44 @@ import styles from './page.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 
-// async function getData() {
-//   const res = await fetch('http://localhost:3000/api/posts', {
-//     cache: 'no-store',
-//   });
+async function getData() {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`, {
+    cache: 'no-store',
+  });
 
-//   if (!res.ok) {
-//     throw new Error('Failed to fetch data');
-//   }
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
 
-//   return res.json();
-// }
+  return res.json();
+}
 
-const Blog = () => {
-  // const data = await getData();
+const Blog = async () => {
+  const data = await getData();
   return (
     <div className={styles.mainContainer}>
-      {/* {data.map((item) => ( */}
-      <Link
-        // href={`/blog/${item._id}`}
-        href="/blog/testId"
-        className={styles.container}
-        // key={item.id}
-      >
-        <div className={styles.imageContainer}>
-          <Image
-            // src={item.img}
-            src="/illustration.png"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>
-            {/* {item.title} */}
-            Test title
-          </h1>
-          <p className={styles.desc}>
-            {/* {item.desc} */}
-            Test paragraph
-          </p>
-        </div>
-      </Link>
-      <Link
-        // href={`/blog/${item._id}`}
-        href="/blog/testId"
-        className={styles.container}
-        // key={item.id}
-      >
-        <div className={styles.imageContainer}>
-          <Image
-            // src={item.img}
-            src="/hero2.png"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>
-            {/* {item.title} */}
-            Test title
-          </h1>
-          <p className={styles.desc}>
-            {/* {item.desc} */}
-            Test paragraph
-          </p>
-        </div>
-      </Link>
-      {/* ))} */}
+      {data.map((item) => (
+        <Link
+          href={`/blog/${item._id}`}
+          className={styles.container}
+          key={item.id}
+        >
+          <div className={styles.imageContainer}>
+            <Image
+              // src={item.img}
+              src="/illustration.png"
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
